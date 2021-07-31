ee6b4511667cdf17259206c64123a2f48b6a7e64
janae-b <janae.boersma@comcast.net>
2021-07-31 00:10:49 -0500

    need to add images

--- components/Books/Books.jsx
+++ components/Books/Books.jsx
@@ -4,7 +4,16 @@ import './Books.css'
 const Books = (props) => {
   return (
     <div className='Books'>
-
+      {props.bookNo.map((book, idx) => 
+      <img
+      src={props.selectedBookIdx === idx ?
+      'image1' :
+      'image2'}
+      alt=''
+      height='100'
+      key={book}
+      />
+      )}
     </div>
   )
 }
